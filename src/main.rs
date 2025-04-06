83e5ce4ac626f03746f4d195cc1e665a12399c51
bibossrou <bibossrou@gmail.com>
2025-04-06 22:25:58 +0200

    Premier commit

---
+++ src/main.rs
@@ -0,0 +1,38 @@
+use std::io;
+
+// Fonction demande d'un prénom
+
+
+fn prenom() {
+    let mut entree = String::new();
+
+    println!("tape ton nom");
+
+    io::stdin()
+        .read_line(&mut entree)
+        .expect("Erreur");
+    
+    println!("tu as entré : {}", entree.trim());
+
+}
+
+
+
+fn main() {
+    let mut input = String::new();
+
+    println!("tape ton age");
+
+    io::stdin()
+        .read_line(&mut input);
+
+    let age : u32 = input.trim().parse().expect("Pas un nombre");
+    
+    if age < 18 {
+        println!("tu es mineur");
+    } else {
+        println!("tu es majeur");
+    }
+
+    
+}
